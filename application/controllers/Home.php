d1de288a798b06938edfa1e8fadcc21052ed2b69
yangbin <619280492@qq.com>
2016-10-21 13:43:48 +0800

    1021_1300

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -29,8 +29,11 @@ class Home extends CI_Controller {
 			$this->count_complex();
 			lineMsg('博物馆综合统计完成');
 
-			$this->count_param();
+			/*$this->count_param();
 			lineMsg('博物馆环境指标综合统计完成');
+
+			$this->count_envtype_param();
+			lineMsg('博物馆达标与稳定情况统计完成');*/
 
 
 
@@ -53,6 +56,7 @@ class Home extends CI_Controller {
 			$data_base['count_precious_relic'] = $this->api->count_precious_relic();
 			$data_base['count_fixed_exhibition'] = $this->api->count_fixed_exhibition();
 			$data_base['count_temporary_exhibition'] = $this->api->count_temporary_exhibition();
+			$data_base['count_showcase'] = $this->api->count_showcase();
 
 			if($this->db->where('mid', $this->museum['id'])->count_all_results('data_base')){
 				$this->db->where('mid', $this->museum['id'])->update('data_base', $data_base);
@@ -74,7 +78,6 @@ class Home extends CI_Controller {
 
 			$data_complex['mid'] = $this->museum['id'];
 			$data_complex['date'] = date("Ymd");
-			$data_complex['standard'] = $this->api->count_standard();
 			$data_complex['scatter_temp'] = $this->api->count_scatter_temp();
 			$data_complex['scatter_humidity'] = $this->api->count_scatter_humidity();
 			$data_complex['is_wave_abnormal'] = $this->api->count_is_wave_abnormal();
@@ -108,15 +111,75 @@ class Home extends CI_Controller {
 			);
 			$mid = $this->museum['id'];
 			foreach($humidity as $k => $v){
-				$paramStr = '('.implode("、",$v).')';
-				$data_param[] = $this->api->count_param_humidity($mid,$k,$paramStr);
+				$data_param[] = $this->api->count_param_humidity($mid,$k);
 
+			}
+			foreach($light as $k => $v){
+				$data_param[] = $this->api->count_param_light($mid,$k);
 			}
 
 			$this->db->insert_batch("data_param",$data_param);
 		}catch (Exception $e){
 			throw new Exception("统计博物馆环境指标综合数据失败！");
 		}
+	}
+
+	// 博物馆达标与稳定情况统计
+	public function count_envtype_param(){
+		try{
+			$data_envtype_param = array();
+			$this->load->library($this->museum['db_type']."/api",array('db'=>$this->subdb));
+
+			//$this->load->config("museum"); //读取湿度、光照配置文件
+			//$humidity = $this->config->item("humidity");
+			//$light = $this->config->item("light");
+			$envArr = array(
+					1=>"展厅",
+					2=>"展柜",
+					3=>"库房"
+			);
+			$humidity = array(
+					1=>array("石质","陶器","瓷器"),
+					2=>array("铁质","青铜"),
+					3=>array("纸质","壁画","纺织品","漆木器")
+			);
+			$light = array(
+					1=>array("石质","陶器","瓷器","铁质","青铜"),
+					2=>array("纸质","壁画","纺织品"),
+					3=>array("漆木器")
+			);
+			$mid = $this->museum['id'];
+
+			//展厅统计温度、光照
+			foreach($humidity as $hum_k => $v){
+					$data_envtype_param[] = $this->api->count_envtype_showroom_humidity($mid,$hum_k);
+			}
+			foreach($light as $lig_k => $v){
+					$data_envtype_param[] = $this->api->count_envtype_showroom_light($mid,$lig_k);
+			}
+
+			//展柜统计温度、光照
+			foreach($humidity as $hum_k => $v){
+				$data_envtype_param[] = $this->api->count_envtype_showcase_humidity($mid,$hum_k);
+			}
+			foreach($light as $lig_k => $v){
+				$data_envtype_param[] = $this->api->count_envtype_showcase_light($mid,$lig_k);
+			}
+
+			//库房统计温度、光照
+			foreach($humidity as $hum_k => $v){
+				$data_envtype_param[] = $this->api->count_envtype_storeroom_humidity($mid,$hum_k);
+			}
+			foreach($light as $lig_k => $v){
+				$data_envtype_param[] = $this->api->count_envtype_storeroom_light($mid,$lig_k);
+			}
+
+
+			$this->db->insert_batch("data_envtype_param",$data_envtype_param);
+		}catch (Exception $e){
+			throw new Exception("统计博物馆达标与稳定情况失败！");
+		}
+
 	}
 
 	// 初始化数据库
--- application/libraries/mysql/api.php
+++ application/libraries/mysql/api.php
@@ -37,13 +37,20 @@ class API{
         return 0;
         return $this->db['relic']->count_all_results('temporary_exhibition');
     }
+    //基础数据-展柜数量
+    public function count_showcase()
+    {
+        /*...*/
+        return 0;
 
-    //综合统计-达标率
+    }
+
+    /*//综合统计-达标率
     public function count_standard()
     {
         //待确认
         return 0;
-    }
+    }*/
 
     //综合统计-温度离散系数
     public function count_scatter_temp()
@@ -74,14 +81,14 @@ class API{
     }
 
     //环境指标综合统计-湿度
-    public function count_param_humidity($mid,$classId,$paramStr){
+    public function count_param_humidity($mid,$classId){
         switch($classId){
             case 1;//("石质","陶器","瓷器")
                 /*......*/
                 return array(
                     "date"=>date("Ymd"),
                     "mid"=>$mid,
-                    "param"=>"湿度".$paramStr,
+                    "param"=>"humidity".$classId,
                     "max"=>0,
                     "min"=>0,
                     "middle"=>0,
@@ -98,7 +105,7 @@ class API{
                 return array(
                     "date"=>date("Ymd"),
                     "mid"=>$mid,
-                    "param"=>"湿度".$paramStr,
+                    "param"=>"humidity".$classId,
                     "max"=>0,
                     "min"=>0,
                     "middle"=>0,
@@ -115,7 +122,7 @@ class API{
                 return array(
                     "date"=>date("Ymd"),
                     "mid"=>$mid,
-                    "param"=>"湿度".$paramStr,
+                    "param"=>"humidity".$classId,
                     "max"=>0,
                     "min"=>0,
                     "middle"=>0,
@@ -133,7 +140,455 @@ class API{
 
     }
 
+    //环境指标综合统计-光照
+    public function count_param_light($mid,$classId)
+    {
+        switch($classId){
+            case 1;//("石质","陶器","瓷器","铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("纸质","壁画","纺织品")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
 
+
+    }
+
+
+    //达标与稳定概况-展厅-湿度
+    public function count_envtype_showroom_humidity($mid,$classId){
+        /*....*/
+        switch($classId){
+            case 1;//("石质","陶器","瓷器")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showroom",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showroom",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("纸质","壁画","纺织品","漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showroom",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    //达标与稳定概况-展厅-光照
+    public function count_envtype_showroom_light($mid,$classId)
+    {
+        /*........*/
+        switch($classId){
+            case 1;//("石质","陶器","瓷器","铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showroom",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("纸质","壁画","纺织品")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showroom",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showroom",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    //达标与稳定概况-展柜-湿度
+    public function count_envtype_showcase_humidity($mid,$classId){
+        /*....*/
+        switch($classId){
+            case 1;//("石质","陶器","瓷器")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showcase",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showcase",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("纸质","壁画","纺织品","漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showcase",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
+    }
+
+    //达标与稳定概况-展柜-光照
+    public function count_envtype_showcase_light($mid,$classId)
+    {
+        switch($classId){
+            case 1;//("石质","陶器","瓷器","铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showcase",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("纸质","壁画","纺织品")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showcase",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"showcase",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    //达标与稳定概况-库房-湿度
+    public function count_envtype_storeroom_humidity($mid,$classId){
+        /*....*/
+        switch($classId){
+            case 1;//("石质","陶器","瓷器")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"storeroom",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"storeroom",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("纸质","壁画","纺织品","漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"storeroom",
+                    "param"=>"humidity".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
+    }
+
+    //达标与稳定概况-库房-光照
+    public function count_envtype_storeroom_light($mid,$classId)
+    {
+        /*.......*/
+        switch($classId){
+            case 1;//("石质","陶器","瓷器","铁质","青铜")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"storeroom",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 2;//("纸质","壁画","纺织品")
+                /*......*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"storeroom",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            case 3;//("漆木器")
+                /*.....*/
+                return array(
+                    "date"=>date("Ymd"),
+                    "mid"=>$mid,
+                    "env_type"=>"storeroom",
+                    "param"=>"light".$classId,
+                    "max"=>0,
+                    "min"=>0,
+                    "middle"=>0,
+                    "average"=>0,
+                    "count_abnormal"=>0,
+                    "max2"=>0,
+                    "min2"=>0,
+                    "standard"=>0,
+                    "compliance"=>0
+                );
+                break;
+            default:
+                break;
+        }
+
+
+    }
 
 
 
