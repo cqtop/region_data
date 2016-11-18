ed36994d4a3c4b19a2d152b997bcd867b17c8dc1
yiluo <1585680807@qq.com>
2016-11-18 11:02:03 +0800

    h

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -24,10 +24,10 @@ class Home extends CI_Controller {
 		try{
 			$this->load->library($this->museum['db_type']."/api", array('db'=>$this->subdb,'mid'=>$this->museum['id']));
 
-//			$this->count_base();
-//			lineMsg('博物馆基础数据统计完成');
-//			$this->count_complex();
-//			lineMsg('博物馆综合统计完成');
+			$this->count_base();
+			lineMsg('博物馆基础数据统计完成');
+			$this->count_complex();
+			lineMsg('博物馆综合统计完成');
 
 			$this->data_envtype_param();
 			lineMsg('环境类型参数综合统计完成');
